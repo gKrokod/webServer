71139ba1d8ca9291cfc9795ff03e6f8c27a8b570
gKrokod <t9013054362@gmail.com>
2024-03-29 21:09:27 +0300

    fist commit in serverPart

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE UndecidableInstances       #-}
-{-# LANGUAGE DataKinds       #-}
+-- {-# LANGUAGE TemplateHaskell            #-}
+-- {-# LANGUAGE QuasiQuotes                #-}
+-- {-# LANGUAGE TypeFamilies               #-}
+-- {-# LANGUAGE UndecidableInstances       #-}
+-- {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE DerivingStrategies #-}
 --
 --
@@ -16,90 +16,20 @@
 
 module Main (main) where
 
-import qualified Data.Text as T
-import Language.Haskell.TH
-import Control.Monad.Reader
-import Control.Monad.Logger 
-import qualified Database.Persist.TH as PTH
-import Database.Persist.Postgresql 
-import Data.Aeson
-import GHC.Generics (Generic)
-import qualified Data.ByteString.Lazy.Char8 as BC
-import qualified Data.ByteString.Lazy as L
-import Config (loadConfigDB, ConfigDB(..))
--- import qualified Data.ByteString.Lazy.Char8 as LC
-import qualified Data.Text.Encoding as E (encodeUtf8)
-import LocalTimeTemplate
+import Network.Wai (responseLBS, Application)
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
 
-import Base.BasicSchema
-import Base.Base-- (Config(..), migrateDB, runAction, createUser, readUser, deleteUser)
-import Base.TestEntity
--- import Data.Tree
--- import Data.Time.Calendar
-import Base.Category
-import Data.Time
-
+import qualified Data.ByteString.Char8 as BC
 
 main :: IO ()
 main = do
-  Prelude.putStrLn "Main start"
-  config <- loadConfigDB
-  case config of 
-    Left decodeError -> print decodeError 
-    Right cfg -> do
-      print "Config DataBase is loaded"
-      -- print cfg
-      let configDB = Config { configConnect = E.encodeUtf8 $ 
-                              mconcat ["host=", cHost $ cfg
-                                      ," port=", cPort $ cfg
-                                      , " user=", cUser $ cfg
-                                      , " dbname=", cDBname $ cfg
-                                      , " password=", cPassword $ cfg]}
-      -- do logic
-      logic (configConnect configDB)      
-  pure ()
+  let host = "127.0.0.1"
+      port = 4221
+  BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack ( show port)
+  run port app
 
-
-logic :: ConnectionString -> IO ()
-logic pginfo = do
--- make tables
---   migrateDB pginfo
--- -- insert Dictionary in DB
---   _ <- insertDictionary pginfo catTr1 
---   dictionary <- fetchDictionary pginfo
--- -- fetch dictionary from DB and insert Categories in DB
---   case dictionary of
---     Nothing -> print "ne nashli dictionary"
---     Just cdict -> do 
---       let spisok = treeToList (categoryDictionaryTree cdict); 
---       insertCategories pginfo spisok
--- -- insert News and Users in DB
---   insertNews pginfo
---   insertUsers pginfo
---
-  filterUser pginfo
-  -- runAction pginfo $ do
-  --   -- people <- selectList [UserData_created ==. fromGregorian 1987 01 01 ] []
-  --   people <- selectList [UserData_created >=. fromGregorian 1200 01 01 ] [Desc UserLogin, LimitTo 6]
-  --   liftIO $ print people
-    -- maybePerson <- getBy $ UniqueLogin "user4"
-    -- case maybePerson of
-    --     Nothing -> liftIO $ putStrLn "Ничего нет"
-    --     Just person -> liftIO $ print person 
-  pure ()
-  putStrLn $ "LocalTime: " <> $(localtimeTemplate)
-
-
-filterUser pginfo = 
-  runAction pginfo $ do
-    -- people <- selectList [UserData_created ==. fromGregorian 1987 01 01 ] []
-    people <- selectList [UserData_created >=. fromGregorian 1200 01 01 ] [Desc UserLogin, LimitTo 6]
-    liftIO $ print people
-
-
-fetchUserhelp pginfo =
-  runAction pginfo $ do
-    maybePerson <- getBy $ UniqueLogin "user4"
-    case maybePerson of
-        Nothing -> liftIO $ putStrLn "Ничего нет"
-        Just person -> liftIO $ print person 
+app :: Application
+app _req f =
+  f $ responseLBS status200 [(hContentType, "text/plain")] "Hello serverok!"
--- src/LocalTimeTemplate.hs
+++
@@ -1,9 +0,0 @@
-module LocalTimeTemplate where
-
-import Language.Haskell.TH
-import Data.Time
-
-localtimeTemplate :: Q Exp
-localtimeTemplate = do
-  t <- runIO getCurrentTime
-  return $ LitE ( StringL (show t) )
