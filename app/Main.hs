68bb5f46a9be66a776ee71c61e2b51d901044457
gKrokod <t9013054362@gmail.com>
2024-04-07 13:05:25 -0700

    think about implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,42 @@
 module Main (main) where
 
+import Control.Exception (bracket_)
 import qualified Handlers.Logger
+import qualified Handlers.WebLogic
 import qualified Logger
-
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (notFound404, status200, status201)
+import Network.HTTP.Types.Header (hContentLength, hContentType, hLocation)
+import Network.Wai (Application, responseBuilder, responseLBS, rawPathInfo)
+import qualified Data.Binary.Builder as BR
 
 greeting :: (Monad m) => Handlers.Logger.Handle m -> m ()
 greeting h = do
   Handlers.Logger.logMessage h Handlers.Logger.Debug "Hello my friend"  
-  
+
+-- old  type Application = Request -> ResourceT IO Response
 --
+-- last type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived -- passing style?
+-- type Application :: Request -> Respond -> IO ResponseReceived
+-- type Respond = Response -> IO ResponseReceived
+app :: Handlers.Logger.Handle IO -> Application
+app h _req f =
+  bracket_
+   (Handlers.Logger.logMessage h Handlers.Logger.Debug "Open app" )
+   (Handlers.Logger.logMessage h Handlers.Logger.Debug "Close app" )
+   (do
+     let handler = Handlers.WebLogic.Handle { Handlers.WebLogic.logger = h, Handlers.WebLogic.buildResponse = responseBuilder }
+     -- response <- Handlers.WebLogic.doWork handler (Handlers.WebLogic.MkRequest (rawPathInfo _req))
+     -- f $ responseBuilder status200 [(hContentType, "text/plain")] (BR.fromByteString $ Handlers.WebLogic.outBody response)
+     a <- (Handlers.WebLogic.doLogic handler (Handlers.WebLogic.MkRequest (rawPathInfo _req)))
+     f a
+     -- response <- f <$> Handlers.WebLogic.doLogic handler (... req)
+     -- f $ response
+     
+   
+   )
+     -- f $ responseLBS status200 [(hContentType, "text/plain")] "status200\n")
+
 main :: IO ()
 main = do
   let logHandle =
@@ -17,5 +45,8 @@ main = do
             Handlers.Logger.writeLog = Logger.writeLog
           }
   greeting logHandle
+-- run :: Port -> Application -> IO () --
+--
+  run 4221 (app logHandle)
   pure ()
 
--- src/Handlers/WebLogic.hs
+++ src/Handlers/WebLogic.hs
@@ -1,26 +1,51 @@
 module Handlers.WebLogic where
 
+import qualified Data.ByteString as B
+import qualified Data.Text as T
 import qualified Handlers.Logger
+import Network.HTTP.Types.Header
+import Network.HTTP.Types
+import Data.Binary.Builder as BR
+import Network.Wai
 
-data Request
-data Response
+data MyRequest = MkRequest { myRawPathInfo :: B.ByteString }
+data MyResponse = MkResponse { outBody :: B.ByteString }
+
+-- data Request
+-- data Response
 
 
 data Handle m = Handle
-  { getRequest :: m (Request)
-  , sendResponse :: Response -> m ()
-  , logger :: Handlers.Logger.Handle m
+  -- { request :: MyRequest 
+  -- , sendResponse :: Response -> m ()
+  { logger :: Handlers.Logger.Handle m,
+    buildResponse :: Status -> ResponseHeaders -> Builder -> Response
   }
 
-doWork :: (Monad m) => Handle m -> m ()
-doWork h = do
+-- old  type Application = Request -> ResourceT IO Response
+--
+-- last type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived -- passing style?
+-- type Application :: Request -> Respond -> IO ResponseReceived
+-- type Respond = Response -> IO ResponseReceived
+--
+doWork :: (Monad m) => Handle m -> MyRequest -> m (MyResponse) 
+-- doWork :: (Monad m) => Handle m -> MyRequest -> m (MyResponse) 
+doWork h req = do
   let logHandle = logger h
   Handlers.Logger.logMessage logHandle Handlers.Logger.Debug "doWork"  
-  req <- getRequest h
   Handlers.Logger.logMessage logHandle Handlers.Logger.Debug "get request"  
-  sendResponse h undefined
+  Handlers.Logger.logMessage logHandle Handlers.Logger.Debug (T.pack $ show $ myRawPathInfo req)
   Handlers.Logger.logMessage logHandle Handlers.Logger.Debug "send response"  
+  pure (MkResponse "my response\n")
 
 
+   -- (f $ responseLBS status200 [(hContentType, "text/plain")] "status200\n")
 
+     -- f $ responseBuilder status200 [(hContentType, "text/plain")] (BR.fromByteString $ Handlers.WebLogic.outBody response)
+
+doLogic :: (Monad m) => Handle m -> MyRequest -> m (Response) 
+-- doWork :: (Monad m) => Handle m -> MyRequest -> m (MyResponse) 
+doLogic h req = do
+  mr <- doWork h req
+  pure $ buildResponse h status200 [(hContentType, "text/plain")] (BR.fromByteString $ outBody mr)
 
