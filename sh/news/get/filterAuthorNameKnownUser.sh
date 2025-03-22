8cb25750e226d5be5e091c694475ac08c6dc0c8e
gKrokod <t9013054362@gmail.com>
2025-03-22 03:43:05 -0700

    test: check. fix: issue24 (turn on compiler flags)

--- sh/news/get/filterAuthorNameKnownUser.sh
+++ sh/news/get/filterAuthorNameKnownUser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-curl -v 'login1:qpass1@127.0.0.1:4221/news?filter=%5B%7B"contents"%3A"user3"%2C"tag"%3A"FilterAuthorName"%7D%5D'
+curl -v 'login1:qpass1@127.0.0.1:4221/news?filter=%5B%7B"contents"%3A"user2"%2C"tag"%3A"FilterAuthorName"%7D%5D'
 
